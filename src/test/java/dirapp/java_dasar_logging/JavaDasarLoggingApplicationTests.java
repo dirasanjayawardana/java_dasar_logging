f1451cf715187b427da3a87e71bb42ed4e4d3047
dirasanjayawardana <dirawardana.dw@gmail.com>
2024-08-07 20:30:14 +0700

    backup

---
+++ src/test/java/dirapp/java_dasar_logging/JavaDasarLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package dirapp.java_dasar_logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDasarLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
